ce96b28d44cc39f622d0513ced7c6112668b8ce2
Surya9122Prakash <sspsurya2002@gmail.com>
2024-01-04 18:43:57 +0530

    updated new deploy

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ mongoose
 
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")));
-app.use(cors());
+// app.use(cors());
 // app.use(cors())
 app.use(cookieParser());
 app.use("/api/auth", authRoute);
