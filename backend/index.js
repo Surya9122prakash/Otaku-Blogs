0aa223ff61c182088eef6a1a0f631422e32e46b0
Surya9122Prakash <sspsurya2002@gmail.com>
2024-01-04 18:06:06 +0530

    deploy commit

---
+++ backend/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const commentRoute = require("./routes/comments");
+
+dotenv.config();
+
+//database
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Database Connected Successfully!");
+  })
+  .catch((error) => {
+    console.error("Error connecting to database:", error);
+  });
+
+app.use(express.json());
+app.use("/images", express.static(path.join(__dirname, "/images")));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cookieParser());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/comments", commentRoute);
+
+app.listen(process.env.PORT, () => {
+  console.log("App is running on Port " + process.env.PORT);
+});
