c2075bc036d226cf1bc0b1fb0d9b1f01b022a776
Surya9122Prakash <sspsurya2002@gmail.com>
2024-01-04 20:25:36 +0530

    updated new deploy

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,6 +5,7 @@ import axios from "axios";
 import { URL } from "../url";
 import { UserContext } from "../context/UserContext";
 import transition from "../components/Transition";
+import { Cookie } from "js-cookie";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -20,7 +21,11 @@ const Login = () => {
         { email, password },
         { withCredentials: true }
       );
-      document.cookie = `token=${res.data.token}; path=/;`;
+      const cookieString = Cookie.get("_HPVN");
+      const startIndex = cookieString.indexOf("eyJQb"); // Find the start of the token
+      const endIndex = cookieString.indexOf("jowfQ") + "jowfQ".length; // Find the end of the token
+      const token = cookieString.substring(startIndex, endIndex);
+      document.cookie = `token=${token}; path=/;`;
       setUser(res.data);
       navigate("/");
     } catch (err) {
